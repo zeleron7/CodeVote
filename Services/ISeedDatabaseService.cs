130b562cf7cd6dd47344a22d264595d40fe48359
zeleron7 <jessie.martensson@hotmail.com>
2025-06-08 16:24:00 +0200

    Implemented endpoints to seed / remove project ideas

---
+++ Services/ISeedDatabaseService.cs
@@ -0,0 +1,10 @@
+﻿using CodeVote.Data;
+
+namespace CodeVote.Services
+{
+    public interface ISeedDatabaseService
+    {
+        public Task SeedDatabaseAsync(CodeVoteContext context);
+        public Task ClearDatabaseAsync(CodeVoteContext context);
+    }
+}
